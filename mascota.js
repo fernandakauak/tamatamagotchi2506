555195addface7758bc1b793848be232491bab61
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-25 22:28:39 -0400

    feat: etapa 2 - metodo describir con this

--- mascota.js
+++ mascota.js
@@ -5,9 +5,13 @@ const mascota = {
   felicidad: 50,   // 0 = triste, 100 = feliz
   energia: 50,     // 0 = agotado, 100 = lleno de energía
 
+  // descripción
+  describir() {
+    return `${this.nombre} es un ${this.tipo}`
+  }
   // En las próximas etapas agregarás métodos AQUÍ dentro.
 };
 
 console.log("Mi tamatama se llama:", mascota.nombre, "y es un", mascota["tipo"]);
 
-
+console.log(mascota.describir());
