abeef55d44a4f9a027bdcaada41629e5c9141475
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-25 22:21:58 -0400

    feat: etapa 1 - objeto mascota

---
+++ mascota.js
@@ -0,0 +1,13 @@
+const mascota = {
+  nombre: "Pelusa",
+  tipo: "gato",
+  hambre: 50,      // 0 = lleno, 100 = muerto de hambre
+  felicidad: 50,   // 0 = triste, 100 = feliz
+  energia: 50,     // 0 = agotado, 100 = lleno de energía
+
+  // En las próximas etapas agregarás métodos AQUÍ dentro.
+};
+
+console.log("Mi tamatama se llama:", mascota.nombre, "y es un", mascota["tipo"]);
+
+
